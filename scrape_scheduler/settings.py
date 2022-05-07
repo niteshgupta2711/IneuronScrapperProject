3f43213f59ed320d2cf747eca898d63f7c6178c4
niteshgupta2711 <88268859+niteshgupta2711@users.noreply.github.com>
2022-05-07 21:24:36 +0530

    Update settings.py

--- scrape_scheduler/settings.py
+++ scrape_scheduler/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)52$sx+7^w%5^zs3*8n=j2!fp@vo=-nbhyi2=h8cmi0g-ssge)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
